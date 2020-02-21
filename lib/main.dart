8065312e5c3b0379064695cd46a43ebb6b92f1f9
JohnathonSouthworth <southworthjohnathon@gmail.com>
2020-02-20 19:54:22 -0800

    created entire app -- was fun

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,49 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blueAccent,
+          appBar: AppBar(
+            backgroundColor: Colors.blueGrey,
+            title: Text('Magic 8 Ball'),
+          ),
+          body: MagicBall(),
+        ),
       ),
     );
+
+class MagicBall extends StatefulWidget {
+  @override
+  _MagicBallState createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int ballAnswer = 1;
+  var rngAnswer = new Random();
+
+  void update8BallAnswer() {
+    setState(() {
+      ballAnswer = rngAnswer.nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Expanded(
+          child: FlatButton(
+            onPressed: () {
+              update8BallAnswer();
+            },
+            child: Image.asset('images/ball$ballAnswer.png'),
+          ),
+        ),
+      ],
+    );
+  }
+}
